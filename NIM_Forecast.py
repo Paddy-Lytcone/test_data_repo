0fde938864abe949cca28d9ef00cca6bf7d8e29b
Paddy <paddy@lytcone.io>
2017-03-07 08:12:06 +0530

    added

--- NIM_Forecast.py
+++ NIM_Forecast.py
@@ -1,6 +1,8 @@
-import csv 
+import csv
 import pandas as pd
 from pandas.stats.api import ols
 df = pd.read_csv('/home/paddy/py_models/PPNR_Models/USNIM.csv')
-results = ols(y=df['NIM'], x= df[['6M' , '1M', '12M', 'CPI']])
+df["Slope"] = df['12M'] - df['1M']
+results = ols(y=df['NIM'], x= df[['1M', 'Slope', 'CPI']])
 print(results)
+
